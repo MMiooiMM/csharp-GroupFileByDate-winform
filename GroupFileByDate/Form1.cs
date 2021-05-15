fd3fd9bcf6a6b8807dd83f5c250aa8d6cd2a2609
Mio <mio093611@gmail.com>
2021-05-15 17:22:42 +0800

    檔案依照日期分組功能

--- GroupFileByDate/Form1.cs
+++ GroupFileByDate/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -17,6 +18,22 @@ namespace GroupFileByDate
             var path = new FolderBrowserDialog();
             path.ShowDialog();
             this.label_path.Text = path.SelectedPath;
+        }
+
+        private void GroupFile(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(this.label_path.Text);
+            var files = di.GetFiles();
+            this.label_file_count_content.Text = files.Length + "";
+            foreach (var group in files.GroupBy(file => file.LastWriteTime.ToShortDateString()))
+            {
+                var basePath = String.Join("_", group.Key.Split("/").Select(x => x.PadLeft(2, '0')));
+                di.CreateSubdirectory(basePath);
+                foreach (var file in group)
+                {
+                    File.Move(file.FullName, Path.Combine(this.label_path.Text, basePath, file.Name));
+                }
+            }
         }
     }
 }
